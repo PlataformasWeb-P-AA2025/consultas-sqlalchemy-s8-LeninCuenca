d562fcd556c0bb88e0bc8d3f29fac79d99ad7867
Lenin <lacuenca16@utpl.edu.ec>
2025-05-27 09:14:26 -0500

    Avance2

--- consulta02.py
+++ consulta02.py
@@ -4,3 +4,23 @@
 # #
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from clases import Base, Departamento, Curso, Tarea, Entrega
+from config import cadena_base_datos
+
+#se arma el camino de la base de datos
+engine = create_engine(cadena_base_datos)
+
+# Crear una sesión
+session = Session(engine)
+
+departamentos = (session.query(Departamento)
+    .join(Departamento.cursos)
+    .join(Curso.tareas)
+    .join(Tarea.entregas)
+    .filter(Entrega.calificacion <= 0.3)
+    .all()
+)
+
+for departamento in departamentos:
+    num_cursos = len(departamento.cursos)
+    print(f"Departamento: {departamento.nombre}, Número de Cursos: {num_cursos}")
